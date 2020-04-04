94dc618cc8c058ba4bd467bcb19851bdbcffdc93
Orry Mevorach <orry.mevorach@loblaw.ca>
2020-04-04 11:48:05 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: 'babel-eslint', // Specifies the ESLint parser
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/no-array-index-key': 'error',
+    'react/no-this-in-sfc': 'error', //Report "this" being used in stateless components
+    'react/no-unused-state': 'error', //Prevent definition of unused state fields
+    'react/destructuring-assignment': 2,
+    'react/prefer-es6-class': 2,
+    'no-console': 1,
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to the Maverick Starter Pack!</h1>
+    </div>
+  );
+}
